3458abb31611b017fee696facd8cbb0b91573608
horchi <vdr@jwendel.de>
2015-10-20 12:04:49 +0200

    fixed field name for backward compatibility

--- update.c
+++ update.c
@@ -105,7 +105,7 @@ cUpdate::~cUpdate() {
 // global field definitions
 
 cDbFieldDef imageSizeDef("MEDIACONTENT", "media_content", cDBS::ffUInt, 0, cDBS::ftData);
-cDbFieldDef uuIDDef("VDRUUID", "vdruuid", cDBS::ffAscii, 40, cDBS::ftData);
+cDbFieldDef uuIDDef("VDRUUID", "uuid", cDBS::ffAscii, 40, cDBS::ftData);
 
 int cUpdate::initDb() 
 {
